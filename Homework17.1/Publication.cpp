68451358b8e925189d8c735ea4b05fe47e440bd3
Misha Bulhakov <mishabulhakov@gmail.com>
2023-04-07 22:00:13 +0300

    Реализация Publication.cpp

--- Homework17.1/Publication.cpp
+++ Homework17.1/Publication.cpp
@@ -1,1 +1,25 @@
 #include "Publication.h"
+
+string Publication::getName() const {
+    return name;
+}
+
+void Publication::setName(const string& name) {
+    this->name = name;
+}
+
+string Publication::getAuthor() const {
+    return author;
+}
+
+void Publication::setAuthor(const string& author) {
+    this->author = author;
+}
+
+int Publication::getPublicationYear() const {
+    return publicationYear;
+}
+
+void Publication::setPublicationYear(int year) {
+    publicationYear = year;
+}
